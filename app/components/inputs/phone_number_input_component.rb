73f7c13d9744c2fba2d0047f7126fbe359d1e724
Koji Shimba <me@shimba.co>
2022-12-03 19:24:50 +0900

    Auto focus

--- app/components/inputs/phone_number_input_component.rb
+++ app/components/inputs/phone_number_input_component.rb
@@ -2,9 +2,10 @@
 # frozen_string_literal: true
 
 class Inputs::PhoneNumberInputComponent < ApplicationComponent
-  sig { params(form: ActionView::Helpers::FormBuilder, field_name: Symbol).void }
-  def initialize(form:, field_name:)
+  sig { params(form: ActionView::Helpers::FormBuilder, field_name: Symbol, autofocus: T::Boolean).void }
+  def initialize(form:, field_name:, autofocus: true)
     @form = form
     @field_name = field_name
+    @autofocus = autofocus
   end
 end
