1b49f441770932b6bad56566785554fc7a5d361b
Koji Shimba <me@shimba.co>
2022-12-10 18:14:09 +0900

    Update migration files

--- db/migrate/20221119004702_create_initial_tables.rb
+++ db/migrate/20221119004702_create_initial_tables.rb
@@ -7,8 +7,9 @@ class CreateInitialTables < ActiveRecord::Migration[7.0]
       t.string :phone_number_origin, null: false
       t.string :confirmation_code, null: false
       t.timestamps
+
+      t.index %i[phone_number confirmation_code], name: :index_phone_number_verifications_on_pn_and_cc, unique: true
     end
-    add_index :phone_number_verifications, %i[phone_number confirmation_code], name: :index_phone_number_verifications_on_pn_and_cc, unique: true
 
     create_table :phone_numbers, id: :uuid do |t|
       t.string :value, index: {unique: true}, null: false
@@ -26,8 +27,9 @@ class CreateInitialTables < ActiveRecord::Migration[7.0]
       t.references :user, foreign_key: true, type: :uuid
       t.references :phone_number, foreign_key: true, type: :uuid
       t.timestamps
+
+      t.index %i[user_id phone_number_id], unique: true
     end
-    add_index :user_phone_numbers, %i[user_id phone_number_id], unique: true
 
     create_table :organizations, id: :uuid do |t|
       t.timestamps
@@ -37,8 +39,9 @@ class CreateInitialTables < ActiveRecord::Migration[7.0]
       t.references :organization, foreign_key: true, null: false, type: :uuid
       t.references :user, foreign_key: true, null: false, type: :uuid
       t.timestamps
+
+      t.index %i[organization_id user_id], unique: true
     end
-    add_index :organization_members, %i[organization_id user_id], unique: true
 
     create_table :profiles, id: :uuid do |t|
       t.integer :profilable_type, null: false
@@ -53,22 +56,25 @@ class CreateInitialTables < ActiveRecord::Migration[7.0]
       t.references :user, foreign_key: true, type: :uuid
       t.references :profile, foreign_key: true, type: :uuid
       t.timestamps
+
+      t.index %i[user_id profile_id], unique: true
     end
-    add_index :user_profiles, %i[user_id profile_id], unique: true
 
     create_table :organization_profiles, id: :uuid do |t|
       t.references :organization, foreign_key: true, type: :uuid
       t.references :profile, foreign_key: true, type: :uuid
       t.timestamps
+
+      t.index %i[organization_id profile_id], unique: true
     end
-    add_index :organization_profiles, %i[organization_id profile_id], unique: true
 
     create_table :follows, id: :uuid do |t|
       t.references :source_profile, foreign_key: {to_table: :profiles}, null: false, type: :uuid
       t.references :target_profile, foreign_key: {to_table: :profiles}, null: false, type: :uuid
       t.timestamps
+
+      t.index %i[source_profile_id target_profile_id], unique: true
     end
-    add_index :follows, %i[source_profile_id target_profile_id], unique: true
 
     create_table :posts, id: :uuid do |t|
       t.references :profile, foreign_key: true, null: false, type: :uuid
--- db/structure.sql
+++ db/structure.sql
@@ -42,6 +42,48 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: active_storage_attachments; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.active_storage_attachments (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name character varying NOT NULL,
+    record_type character varying NOT NULL,
+    record_id uuid NOT NULL,
+    blob_id uuid NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: active_storage_blobs; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.active_storage_blobs (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    key character varying NOT NULL,
+    filename character varying NOT NULL,
+    content_type character varying,
+    metadata text,
+    service_name character varying NOT NULL,
+    byte_size bigint NOT NULL,
+    checksum character varying,
+    created_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: active_storage_variant_records; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.active_storage_variant_records (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    blob_id uuid NOT NULL,
+    variation_digest character varying NOT NULL
+);
+
+
+--
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -208,6 +250,30 @@ CREATE TABLE public.users (
 
 
 --
+-- Name: active_storage_attachments active_storage_attachments_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.active_storage_attachments
+    ADD CONSTRAINT active_storage_attachments_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: active_storage_blobs active_storage_blobs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.active_storage_blobs
+    ADD CONSTRAINT active_storage_blobs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: active_storage_variant_records active_storage_variant_records_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.active_storage_variant_records
+    ADD CONSTRAINT active_storage_variant_records_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -309,6 +375,34 @@ ALTER TABLE ONLY public.user_profiles
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_active_storage_attachments_on_blob_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_active_storage_attachments_on_blob_id ON public.active_storage_attachments USING btree (blob_id);
+
+
+--
+-- Name: index_active_storage_attachments_uniqueness; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_active_storage_attachments_uniqueness ON public.active_storage_attachments USING btree (record_type, record_id, name, blob_id);
+
+
+--
+-- Name: index_active_storage_blobs_on_key; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_active_storage_blobs_on_key ON public.active_storage_blobs USING btree (key);
+
+
+--
+-- Name: index_active_storage_variant_records_uniqueness; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX index_active_storage_variant_records_uniqueness ON public.active_storage_variant_records USING btree (blob_id, variation_digest);
 
 
 --
@@ -493,6 +587,14 @@ ALTER TABLE ONLY public.user_profiles
 
 
 --
+-- Name: active_storage_variant_records fk_rails_993965df05; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.active_storage_variant_records
+    ADD CONSTRAINT fk_rails_993965df05 FOREIGN KEY (blob_id) REFERENCES public.active_storage_blobs(id);
+
+
+--
 -- Name: organization_members fk_rails_a0a760b9b4; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -506,6 +608,14 @@ ALTER TABLE ONLY public.organization_members
 
 ALTER TABLE ONLY public.follows
     ADD CONSTRAINT fk_rails_abf061b756 FOREIGN KEY (target_profile_id) REFERENCES public.profiles(id);
+
+
+--
+-- Name: active_storage_attachments fk_rails_c3b3935057; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.active_storage_attachments
+    ADD CONSTRAINT fk_rails_c3b3935057 FOREIGN KEY (blob_id) REFERENCES public.active_storage_blobs(id);
 
 
 --
@@ -540,6 +650,7 @@ SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20220000000001'),
-('20221119004702');
+('20221119004702'),
+('20221210090702');
 
 
