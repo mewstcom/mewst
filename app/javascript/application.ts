1232bcaf83b344e2f7b3eb75c6d0b4b7b8d4f59a
Koji Shimba <me@shimba.co>
2023-01-02 20:07:57 +0900

    Make transition to post page simple

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -7,9 +7,7 @@ import AutosizeController from './controllers/autosize-controller';
 import ComponentDataFetcherController from './controllers/component-data-fetcher-controller';
 import FollowButtonController from './controllers/follow-button-controller';
 import PhoneNumberInputController from "./controllers/phone-number-input-controller";
-import PostCardController from "./controllers/post-card-controller";
 import PostFormController from "./controllers/post-form-controller";
-import StickyPostController from './controllers/sticky-post-controller';
 import TimelineController from "./controllers/timeline-controller";
 
 window.Stimulus = Application.start();
@@ -17,9 +15,7 @@ Stimulus.register('autosize', AutosizeController);
 Stimulus.register('component-data-fetcher', ComponentDataFetcherController);
 Stimulus.register('follow-button', FollowButtonController);
 Stimulus.register('phone-number-input', PhoneNumberInputController);
-Stimulus.register('post-card', PostCardController);
 Stimulus.register('post-form', PostFormController);
-Stimulus.register('sticky-post', StickyPostController);
 Stimulus.register('timeline', TimelineController);
 
 ujs.start();
