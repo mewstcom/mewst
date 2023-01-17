55de8d258b03e9e72ef9dbfca40e95392f3937d4
Koji Shimba <me@shimba.co>
2023-01-18 04:50:44 +0900

    Rename environment variables

--- app/lib/mewst/redis.rb
+++ app/lib/mewst/redis.rb
@@ -7,7 +7,7 @@ class Mewst::Redis
   sig { returns(T.any(ConnectionPool::Wrapper, Redis)) }
   def self.client
     @redis ||= T.let(ConnectionPool::Wrapper.new do
-      Redis.new(url: ENV.fetch("MEWST_REDIS_CACHE_URL"))
+      Redis.new(url: ENV.fetch("MEWST_REDIS_UNEVICTABLE_CACHE_URL"))
     end, T.any(T.nilable(ConnectionPool::Wrapper), T.nilable(Redis)))
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV.fetch("MEWST_REDIS_SIDEKIQ_URL")}
+  config.redis = {url: ENV.fetch("MEWST_REDIS_WORKER_URL")}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: ENV.fetch("MEWST_REDIS_SIDEKIQ_URL")}
+  config.redis = {url: ENV.fetch("MEWST_REDIS_WORKER_URL")}
 end
