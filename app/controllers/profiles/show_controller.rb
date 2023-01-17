5e911e41e6d33bf1d4b7a241498f2e63e225ef38
Koji Shimba <me@shimba.co>
2023-01-18 04:51:59 +0900

    Fix profile page which guest user can access

--- app/controllers/profiles/show_controller.rb
+++ app/controllers/profiles/show_controller.rb
@@ -7,7 +7,6 @@ class Profiles::ShowController < ApplicationController
   include Localizable
 
   around_action :set_locale
-  before_action :require_authentication
 
   sig { returns(T.untyped) }
   def call
