1ea58aebdbad683c6c89df50cbcc8b7e9de6d4c6
Koji Shimba <me@shimba.co>
2023-04-08 21:13:20 +0900

    Update styles

--- app/components/images/avatar_image_component.rb
+++ app/components/images/avatar_image_component.rb
@@ -2,11 +2,12 @@
 # frozen_string_literal: true
 
 class Images::AvatarImageComponent < ApplicationComponent
-  sig { params(profile: Profile, width: Integer, alt: String).void }
-  def initialize(profile:, width:, alt: "")
+  sig { params(profile: Profile, width: Integer, alt: String, class_name: String).void }
+  def initialize(profile:, width:, alt: "", class_name: "")
     @profile = profile
     @width = width
     @alt = T.let(alt.presence || "@#{@profile.atname}", String)
+    @class_name = class_name
   end
 
   private
@@ -14,5 +15,12 @@ class Images::AvatarImageComponent < ApplicationComponent
   sig { returns(String) }
   def avatar_url
     @profile.avatar_url.presence || asset_url("avatar.png")
+  end
+
+  sig { returns(String) }
+  def class_name
+    class_list = %w[rounded-circle]
+    class_list << @class_name if @class_name.present?
+    class_list.join(" ")
   end
 end
