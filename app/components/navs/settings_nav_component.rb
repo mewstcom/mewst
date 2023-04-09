54b3e40aa5f83ead1db3ba5766c06a4825f56bd8
Koji Shimba <me@shimba.co>
2023-04-09 17:19:44 +0900

    Add settings page

--- app/components/navs/settings_nav_component.rb
+++
@@ -1,9 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-class Navs::SettingsNavComponent < ApplicationComponent
-  sig { params(path: String).returns(String) }
-  def nav_link_class_names(path)
-    class_names("nav-link", {active: current_page?(path)})
-  end
-end
