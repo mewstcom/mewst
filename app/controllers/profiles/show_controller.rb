f1214eca1ee9313b05da05c2d4d6f87e2ad6854c
Koji Shimba <me@shimba.co>
2023-04-09 14:48:04 +0900

    Use `published_at` for sort

--- app/controllers/profiles/show_controller.rb
+++ app/controllers/profiles/show_controller.rb
@@ -13,6 +13,6 @@ class Profiles::ShowController < ApplicationController
   sig { returns(T.untyped) }
   def call
     @profile = Profile.only_kept.find_by!(atname: params[:atname])
-    @pagy, @entries = pagy(@profile.entries.order(created_at: :desc))
+    @pagy, @entries = pagy(@profile.entries.order(published_at: :desc))
   end
 end
