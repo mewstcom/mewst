b71983018e5f21844e47095367ef4f54139a9bad
Koji Shimba <me@shimba.co>
2023-05-29 05:26:20 +0900

    Add database_id field

--- app/graphql/internal/types/objects/oauth_access_token.rb
+++ app/graphql/internal/types/objects/oauth_access_token.rb
@@ -2,5 +2,10 @@
 # frozen_string_literal: true
 
 class Internal::Types::Objects::OauthAccessToken < Internal::Types::Objects::Base
+  field :database_id, String, null: false
   field :token, String, null: false
+
+  def database_id
+    object.id
+  end
 end
