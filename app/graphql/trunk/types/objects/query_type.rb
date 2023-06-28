9e5fe4abe53d4ca41094edafe3e9be483a9dd916
Koji Shimba <me@shimba.co>
2023-06-28 23:37:16 +0900

    Move `profile` field to QueryType

--- app/graphql/trunk/types/objects/query_type.rb
+++ app/graphql/trunk/types/objects/query_type.rb
@@ -2,9 +2,17 @@
 # frozen_string_literal: true
 
 class Trunk::Types::Objects::QueryType < Trunk::Types::Objects::Base
+  field :profile, Trunk::Types::Objects::ProfileType, null: true do
+    argument :atname, String, required: true
+  end
+
   field :viewer, Trunk::Types::Objects::UserType,
         null: false,
         description: "The currently authenticated user."
+
+  def profile(atname:)
+    Profile.find_by(atname:)
+  end
 
   def viewer
     context[:viewer]
--- app/graphql/trunk/types/objects/user_type.rb
+++ app/graphql/trunk/types/objects/user_type.rb
@@ -5,15 +5,8 @@ class Trunk::Types::Objects::UserType < Trunk::Types::Objects::Base
   implements GraphQL::Types::Relay::Node
 
   field :locale, Trunk::Types::Enums::Locale, null: false
-  field :profile, Trunk::Types::Objects::ProfileType, null: true do
-    argument :atname, String, required: true
-  end
 
   def locale
     object.locale.upcase
-  end
-
-  def profile(atname:)
-    object.profiles.find_by(atname:)
   end
 end
