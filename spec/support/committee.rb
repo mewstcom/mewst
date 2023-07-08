518aecb200e8a6482ca4dbcc4e7d3f75afebbd9e
Koji Shimba <me@shimba.co>
2023-07-08 15:28:30 +0900

    Setup Committee for testing

---
+++ spec/support/committee.rb
@@ -0,0 +1,19 @@
+# typed: false
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include Committee::Rails::Test::Methods
+  config.add_setting :committee_options
+  config.committee_options = {
+    query_hash_key: "rack.request.query_hash",
+    parse_response_by_content_type: false,
+    strict_reference_validation: true
+  }
+
+  config.before :all, api_version: :latest do
+    RSpec.configure do |config|
+      config.committee_options[:schema_path] = Rails.root.join("app", "controllers", "latest", "openapi.yaml").to_s
+      config.committee_options[:prefix] = "/latest"
+    end
+  end
+end
