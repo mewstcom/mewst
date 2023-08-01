11bfb6e54478f6899f983423fc0ed8dbd8fab551
Koji Shimba <me@shimba.co>
2023-08-02 01:54:00 +0900

    Fix spec

--- spec/requests/latest/timeline/show_spec.rb
+++ spec/requests/latest/timeline/show_spec.rb
@@ -7,11 +7,8 @@ RSpec.describe "GET /latest/@:atname/timeline", type: :request, api_version: :la
     let!(:profile) { user.first_profile }
     let!(:oauth_access_token) { user.oauth_access_tokens.first }
     let!(:headers) { {"Authorization" => "bearer #{oauth_access_token.token}"} }
-
-    before do
-      form = Forms::CommentedPost.new(comment: "Hello", profile:)
-      Services::CreateCommentedPost.new(form:).call
-    end
+    let!(:form) { Forms::CommentedPost.new(comment: "Hello", profile:) }
+    let!(:post) { Services::CreateCommentedPost.new(form:).call.post }
 
     it "returns posts on timeline" do
       get("/latest/@#{profile.atname}/timeline", headers:)
@@ -19,7 +16,32 @@ RSpec.describe "GET /latest/@:atname/timeline", type: :request, api_version: :la
       expect(response).to have_http_status(:ok)
       assert_response_schema_confirm(200)
 
-      expect(response.header["Link"]).to eq("")
+      expected = {
+        posts: [
+          {
+            id: post.id,
+            postable: {
+              comment: "Hello"
+            },
+            postable_type: "commented_post",
+            profile: {
+              atname: profile.atname,
+              avatar_url: profile.avatar_url,
+              name: profile.name
+            },
+            published_at: post.published_at.iso8601(3),
+            reposts_count: 0
+          }
+        ],
+        page_info: {
+          has_next_page: false,
+          has_previous_page: false,
+          start_cursor: nil,
+          end_cursor: post.id
+        }
+      }
+      actual = JSON.parse(response.body)
+      expect(actual).to include(expected.deep_stringify_keys)
     end
   end
 end
