68f2114b51239405e91c9cec6ac5849577c6f4b8
Koji Shimba <me@shimba.co>
2023-08-05 01:21:43 +0900

    Enable Bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,4 +82,13 @@ Rails.application.configure do
   config.hosts += [
     ENV.fetch("MEWST_HOST")
   ]
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
