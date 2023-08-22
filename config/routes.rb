0bba8b4d39d2849e699cfa75a742480ff171cb0d
Koji Shimba <me@shimba.co>
2023-08-23 01:46:22 +0900

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,10 @@ Rails.application.routes.draw do
   match "/latest/@:atname/follow",                                        via: :delete, as: :latest_follow,                                   to: "latest/follows/destroy#call",                         atname: ROUTING_USERNAME_FORMAT
   match "/latest/@:atname/follow",                                        via: :post,                                                         to: "latest/follows/create#call",                          atname: ROUTING_USERNAME_FORMAT
   match "/latest/@:atname/posts",                                         via: :get,    as: :latest_profile_post_list,                        to: "latest/profiles/posts/index#call",                    atname: ROUTING_USERNAME_FORMAT
-  match "/latest/timeline",                                               via: :get,    as: :latest_timeline,                                 to: "latest/timeline/show#call"
+  match "/latest/me",                                                     via: :get,    as: :latest_me,                                       to: "latest/me/show#call"
   match "/latest/posts",                                                  via: :post,   as: :latest_post_list,                                to: "latest/posts/create#call"
   match "/latest/posts/:post_id/stamp",                                   via: :delete, as: :latest_post_stamp,                               to: "latest/stamps/destroy#call"
   match "/latest/posts/:post_id/stamp",                                   via: :post,                                                         to: "latest/stamps/create#call"
+  match "/latest/timeline",                                               via: :get,    as: :latest_timeline,                                 to: "latest/timeline/show#call"
   # standard:enable Layout/ExtraSpacing, Rails/MatchRoute
 end
