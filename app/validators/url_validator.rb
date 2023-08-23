5d747fce3c8d114163b3a1996c109ebd3a817232
Koji Shimba <me@shimba.co>
2023-08-24 01:19:19 +0900

    Fix

---
+++ app/validators/url_validator.rb
@@ -0,0 +1,23 @@
+# typed: strict
+# frozen_string_literal: true
+
+class UrlValidator < ActiveModel::EachValidator
+  extend T::Sig
+
+  sig { params(record: Latest::ApplicationForm, attribute: Symbol, value: String).void }
+  def validate_each(record, attribute, value)
+    return if value.blank?
+
+    unless valid_uri?(value)
+      record.errors.add(attribute, :url)
+    end
+  end
+
+  sig { params(value: String).returns(T::Boolean) }
+  private def valid_uri?(value)
+    uri = Addressable::URI.parse(value)
+    uri.is_a?(Addressable::URI) && !uri.host.nil?
+  rescue Addressable::URI::InvalidURIError
+    false
+  end
+end
