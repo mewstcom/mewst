07431526c64b45e08f29b238266018f2b3dc571b
Koji Shimba <me@shimba.co>
2024-01-13 06:08:16 +0900

    シードデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@ OauthApplication.where(uid: OauthApplication::MEWST_WEB_UID).first_or_create!(
   redirect_uri: "https://example.com/callback",
   scopes: ""
 )
-
-[
-  %w[me@shimba.co ja shimbaco password https://shimba.co/img/shimbaco.jpg],
-  ["user1@example.com", "ja", "user1", "password", ""],
-  ["user2@example.com", "ja", "user2", "password", ""],
-  ["user3@example.com", "ja", "user3", "password", ""]
-].each do |(email, locale, atname, password, avatar_url)|
-  CreateSeedAccountUseCase.new.call(atname:, email:, locale:, password:, avatar_url:)
-end
