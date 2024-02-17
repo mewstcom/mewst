b4d30a86d2afb86abd5566df8369421136d82abb
Koji Shimba <me@shimba.co>
2024-02-17 13:52:47 +0900

    `turbo-frame` 要素を `turbo_frame_tag` ヘルパーに置き換える

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -4,5 +4,8 @@
 class ApplicationComponent < ViewComponent::Base
   extend T::Sig
 
+  # ref: https://github.com/ViewComponent/view_component/issues/1099
+  include Turbo::FramesHelper
+
   delegate :current_actor!, :mst_absolute_time, :mst_image_url, :mst_time_ago_in_words, :signed_in?, to: :helpers
 end
