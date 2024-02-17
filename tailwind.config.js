62a48634487298eb5b9273dde95621305f88b6da
Koji Shimba <me@shimba.co>
2024-02-17 19:33:47 +0900

    `app/components` 以下のファイルも対象にする

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/*.html', './app/helpers/**/*.rb', './app/javascript/**/*.ts', './app/views/**/*.erb'],
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.ts',
+    './app/components/**/*.erb',
+    './app/views/**/*.erb',
+  ],
   theme: {
     extend: {},
   },
