718ada0cd979881cd9df1d47239cbe49bb70e845
Koji Shimba <me@shimba.co>
2024-03-16 22:26:49 +0900

    ポスト投稿ページのスタイルを調整

--- app/components/layouts/basic_layout_component.rb
+++ app/components/layouts/basic_layout_component.rb
@@ -2,12 +2,17 @@
 # frozen_string_literal: true
 
 class Layouts::BasicLayoutComponent < ApplicationComponent
-  sig { params(with_footer: T::Boolean).void }
-  def initialize(with_footer: true)
+  sig { params(with_footer: T::Boolean, main_class_name: String).void }
+  def initialize(with_footer: true, main_class_name: "")
     @with_footer = with_footer
+    @main_class_name = main_class_name
   end
 
   sig { returns(T::Boolean) }
   attr_reader :with_footer
   private :with_footer
+
+  sig { returns(String) }
+  attr_reader :main_class_name
+  private :main_class_name
 end
