f65b5c977bc065723a522c322277783338b06fcc
Koji Shimba <me@shimba.co>
2024-04-05 04:14:46 +0900

    IPアドレスとUAは取得できないことがある

--- app/models/session.rb
+++ app/models/session.rb
@@ -8,8 +8,12 @@ class Session < ApplicationRecord
 
   belongs_to :actor
 
-  sig { params(ip_address: String, user_agent: String, signed_in_at: ActiveSupport::TimeWithZone).returns(Session) }
+  sig { params(ip_address: T.nilable(String), user_agent: T.nilable(String), signed_in_at: ActiveSupport::TimeWithZone).returns(Session) }
   def self.start!(ip_address:, user_agent:, signed_in_at: Time.current)
-    create!(ip_address:, user_agent:, signed_in_at:)
+    create!(
+      ip_address: ip_address || "",
+      user_agent: user_agent || "",
+      signed_in_at:
+    )
   end
 end
--- app/use_cases/create_session_use_case.rb
+++ app/use_cases/create_session_use_case.rb
@@ -6,7 +6,7 @@ class CreateSessionUseCase < ApplicationUseCase
     const :session, Session
   end
 
-  sig { params(actor: Actor, ip_address: String, user_agent: String).returns(Result) }
+  sig { params(actor: Actor, ip_address: T.nilable(String), user_agent: T.nilable(String)).returns(Result) }
   def call(actor:, ip_address:, user_agent:)
     session = ActiveRecord::Base.transaction do
       actor.sessions.start!(ip_address:, user_agent:)
