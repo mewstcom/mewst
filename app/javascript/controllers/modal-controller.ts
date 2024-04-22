eec3cd6e5dd99932013c100cc1b5aef4c735dc32
Koji Shimba <me@shimba.co>
2024-04-22 23:18:01 +0900

    モーダルの中でリンクカードを追加したときモーダルを閉じないようにする

--- app/javascript/controllers/modal-controller.ts
+++ app/javascript/controllers/modal-controller.ts
@@ -6,6 +6,11 @@ export default class extends Controller<HTMLDialogElement> {
   }
 
   close(event: CustomEvent) {
+    // モーダルの中でリンクカードを追加したときモーダルを閉じないようにする
+    if (event.target.dataset.action !== 'turbo:submit-end->modal#close') {
+      return;
+    }
+
     if (event.detail.success) {
       this.element.close();
     }
