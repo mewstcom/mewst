b7b88e7f904ee19e58cea374d68780bbaf562ee7
Koji Shimba <me@shimba.co>
2024-04-30 00:25:28 +0900

    使われていない component-data-fetcher を除去

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -4,7 +4,6 @@ import { Application } from '@hotwired/stimulus';
 
 import AutosizeController from './controllers/autosize-controller';
 import CharacterCounterController from './controllers/character-counter-controller';
-import ComponentDataFetcherController from './controllers/component-data-fetcher-controller';
 import DropdownController from './controllers/dropdown-controller';
 import FlashToastController from './controllers/flash-toast-controller';
 import FlashToastDispatchController from './controllers/flash-toast-dispatch-controller';
@@ -18,7 +17,6 @@ window.Stimulus = application;
 
 Stimulus.register('autosize', AutosizeController);
 Stimulus.register('character-counter', CharacterCounterController);
-Stimulus.register('component-data-fetcher', ComponentDataFetcherController);
 Stimulus.register('dropdown', DropdownController);
 Stimulus.register('flash-toast', FlashToastController);
 Stimulus.register('flash-toast-dispatch', FlashToastDispatchController);
