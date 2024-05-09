ed9a81d395e18aa8c33c57888bb1c2811432e72f
Koji Shimba <me@shimba.co>
2024-05-10 02:10:38 +0900

    `bin/standardrb --fix` を実行

--- config/initializers/rails_live_reload.rb
+++ config/initializers/rails_live_reload.rb
@@ -1,15 +1,17 @@
 # typed: strict
 # frozen_string_literal: true
 
-RailsLiveReload.configure do |config|
-  config.url = "/rails/live/reload"
+if defined?(RailsLiveReload)
+  RailsLiveReload.configure do |config|
+    config.url = "/rails/live/reload"
 
-  # Default watched folders & files
-  config.watch %r{app/components/.+\.(erb|rb)$}, reload: :on_change
-  config.watch %r{app/views/.+\.erb$}, reload: :on_change
-  config.watch %r{(app|vendor)/(assets|javascript)/\w+/(.+\.(css|js|html|png|jpg|ts)).*}, reload: :always
-  config.watch %r{app/helpers/.+\.rb}, reload: :always
-  config.watch %r{config/locales/.+\.yml}, reload: :always
+    # Default watched folders & files
+    config.watch %r{app/components/.+\.(erb|rb)$}, reload: :on_change
+    config.watch %r{app/views/.+\.erb$}, reload: :on_change
+    config.watch %r{(app|vendor)/(assets|javascript)/\w+/(.+\.(css|js|html|png|jpg|ts)).*}, reload: :always
+    config.watch %r{app/helpers/.+\.rb}, reload: :always
+    config.watch %r{config/locales/.+\.yml}, reload: :always
 
-  config.enabled = Rails.env.development?
-end if defined?(RailsLiveReload)
+    config.enabled = Rails.env.development?
+  end
+end
