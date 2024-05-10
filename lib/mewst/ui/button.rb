df78e20bfbb8dafef8d74a2aa4eed248b1d48d18
Koji Shimba <me@shimba.co>
2024-05-11 05:04:53 +0900

    型を修正

--- lib/mewst/ui/button.rb
+++ lib/mewst/ui/button.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 class Mewst::UI::Button < Mewst::UI::Base
-  sig { params(as: Symbol, class_name: String, options: T::Hash[Symbol, String]).void }
+  sig { params(as: Symbol, class_name: String, options: T::Hash[Symbol, T.anything]).void }
   def initialize(as: :button, class_name: "", **options)
     @as = as
     @class_name = class_name
@@ -17,7 +17,7 @@ class Mewst::UI::Button < Mewst::UI::Base
   attr_reader :class_name
   private :class_name
 
-  sig { returns(T::Hash[Symbol, String]) }
+  sig { returns(T::Hash[Symbol, T.anything]) }
   attr_reader :options
   private :options
 end
