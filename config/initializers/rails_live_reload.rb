d9357cadb4e9884da6369a2c20418cc96abb4816
Koji Shimba <me@shimba.co>
2024-05-11 04:30:11 +0900

    Rails Live Reloadの設定を修正

--- config/initializers/rails_live_reload.rb
+++ config/initializers/rails_live_reload.rb
@@ -6,7 +6,7 @@ if defined?(RailsLiveReload)
     config.url = "/rails/live/reload"
 
     # Default watched folders & files
-    config.watch %r{app/components/.+\.(erb|rb)$}, reload: :on_change
+    config.watch %r{app/components/.+\.(erb|rb)$}, reload: :always
     config.watch %r{app/views/.+\.erb$}, reload: :on_change
     config.watch %r{(app|vendor)/(assets|javascript)/\w+/(.+\.(css|js|html|png|jpg|ts)).*}, reload: :always
     config.watch %r{app/helpers/.+\.rb}, reload: :always
