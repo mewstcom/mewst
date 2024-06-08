ad904f6abef86485eae3040e0186887f6925acfc
Koji Shimba <me@shimba.co>
2024-06-08 21:38:35 +0900

    `Mewst::UI::Icon` でSVG画像を使うように修正

--- lib/mewst/ui/icon.rb
+++ lib/mewst/ui/icon.rb
@@ -2,15 +2,20 @@
 # frozen_string_literal: true
 
 class Mewst::UI::Icon < Mewst::UI::Base
-  sig { params(name: String, class_name: String).void }
-  def initialize(name:, class_name: "")
+  sig { params(name: String, size: String, class_name: String).void }
+  def initialize(name:, size: "16px", class_name: "")
     @name = name
+    @size = size
     @class_name = class_name
   end
 
   sig { returns(String) }
   attr_reader :name
   private :name
+
+  sig { returns(String) }
+  attr_reader :size
+  private :size
 
   sig { returns(String) }
   attr_reader :class_name
