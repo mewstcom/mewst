d6d2f17fb3a8d41c152c1f20bbfe288394ba4b9e
Koji Shimba <me@shimba.co>
2024-06-28 19:09:54 +0900

    タブのアイテムをリンクにする

--- lib/mewst/ui/tabs/item.rb
+++ lib/mewst/ui/tabs/item.rb
@@ -2,11 +2,16 @@
 # frozen_string_literal: true
 
 class Mewst::UI::Tabs::Item < Mewst::UI::Base
-  sig { params(active: T::Boolean, class_name: String).void }
-  def initialize(active: false, class_name: "")
+  sig { params(href: String, active: T::Boolean, class_name: String).void }
+  def initialize(href:, active: false, class_name: "")
+    @href = href
     @active = active
     @class_name = class_name
   end
+
+  sig { returns(String) }
+  attr_reader :href
+  private :href
 
   sig { returns(T::Boolean) }
   attr_reader :active
