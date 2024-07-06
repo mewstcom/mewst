c1c7415ef7231d7fbc8380da40bff21eabac0e02
Koji Shimba <me@shimba.co>
2024-07-06 21:09:17 +0900

    `/manifest.json` だけリクエストを受けるように修正
    Fix:
    https://mewst.sentry.io/issues/5246787396/events/fa85f2cb3a94409eabe1992ba1614d6e/

--- app/controllers/manifests/show_controller.rb
+++ app/controllers/manifests/show_controller.rb
@@ -4,6 +4,9 @@
 class Manifests::ShowController < ApplicationController
   sig { returns(T.untyped) }
   def call
-    render(layout: false)
+    respond_to do |format|
+      format.json { render(layout: false) }
+      format.any  { render(plain: "Not Found", status: :not_found) }
+    end
   end
 end
