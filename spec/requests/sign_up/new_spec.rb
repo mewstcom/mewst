02f4a281984673797bca0c41f5b959a57536d481
Koji Shimba <me@shimba.co>
2025-07-20 03:06:22 +0900

    Fix

--- spec/requests/sign_up/new_spec.rb
+++ spec/requests/sign_up/new_spec.rb
@@ -2,25 +2,51 @@
 # frozen_string_literal: true
 
 RSpec.describe "GET /sign_up", type: :request do
-  context "ログインしているとき" do
-    let!(:actor) { FactoryBot.create(:actor) }
+  it "ログインしているとき、ホーム画面にリダイレクトすること" do
+    actor = FactoryBot.create(:actor)
+    sign_in actor
 
-    before do
-      sign_in actor
-    end
+    get "/sign_up"
 
-    it "ホーム画面にリダイレクトすること" do
-      get "/sign_up"
-
-      expect(response).to redirect_to(home_path)
-    end
+    expect(response).to redirect_to(home_path)
   end
 
-  context "ログインしていないとき" do
-    it "ページが表示されること" do
-      get "/sign_up"
+  it "ログインしていないとき、ページが表示されること" do
+    get "/sign_up"
 
-      expect(response.body).to include("アカウント作成")
-    end
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include("アカウント作成")
+  end
+
+  it "ログインしていないとき、メールアドレス入力フォームが表示されること" do
+    get "/sign_up"
+
+    expect(response.body).to include("メールアドレス")
+    expect(response.body).to include('type="email"')
+    expect(response.body).to include('name="email_confirmation_form[email]"')
+  end
+
+  it "ログインしていないとき、ログインリンクが表示されること" do
+    get "/sign_up"
+
+    expect(response.body).to include("アカウント持ってる？")
+  end
+
+  it "サインアップが停止されているとき、フォームが無効化されること" do
+    allow(SignUpStopper).to receive(:enabled?).and_return(true)
+
+    get "/sign_up"
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include('disabled="disabled"')
+  end
+
+  it "サインアップが停止されていないとき、フォームが有効であること" do
+    allow(SignUpStopper).to receive(:enabled?).and_return(false)
+
+    get "/sign_up"
+
+    expect(response).to have_http_status(:ok)
+    expect(response.body).not_to include('disabled="disabled"')
   end
 end
