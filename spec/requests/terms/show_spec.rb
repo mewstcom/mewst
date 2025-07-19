83148d48ec4856fa6995469b486975aebbcf78fa
Koji Shimba <me@shimba.co>
2025-07-20 03:22:04 +0900

    Fix

---
+++ spec/requests/terms/show_spec.rb
@@ -0,0 +1,24 @@
+# typed: true
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "GET /terms", type: :request do
+  it "利用規約のURLにリダイレクトすること" do
+    terms_url = "https://example.com/terms"
+    allow(Rails.configuration.mewst).to receive(:[]).with("terms_url").and_return(terms_url)
+
+    get "/terms"
+
+    expect(response).to redirect_to(terms_url)
+  end
+
+  it "外部ホストへのリダイレクトを許可すること" do
+    terms_url = "https://external-domain.com/terms"
+    allow(Rails.configuration.mewst).to receive(:[]).with("terms_url").and_return(terms_url)
+
+    get "/terms"
+
+    expect(response).to redirect_to(terms_url)
+  end
+end
