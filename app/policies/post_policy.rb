797989018fed80628ae660806e7366bcc9613539
Koji Shimba <me@shimba.co>
2025-08-02 13:00:31 +0900

    Fix

--- app/policies/post_policy.rb
+++
@@ -1,9 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-class PostPolicy < ApplicationPolicy
-  sig { returns(T::Boolean) }
-  def destroy?
-    profile == T.cast(record, PostRecord).profile_record
-  end
-end
--- app/policies/profile_policy.rb
+++
@@ -1,9 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-class ProfilePolicy < ApplicationPolicy
-  sig { returns(T::Boolean) }
-  def show?
-    profile == T.cast(record, ProfileRecord)
-  end
-end
